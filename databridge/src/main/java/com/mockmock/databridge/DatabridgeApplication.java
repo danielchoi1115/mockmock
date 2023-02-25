e0dbac92e57215ac1f69dcd766f21dc66d3bdbe6
CHOI, Seung Ryeol <schoiak@connect.ust.hk>
2023-02-25 20:33:08 +0900

    Add - Data Bridge added

---
+++ databridge/src/main/java/com/mockmock/databridge/DatabridgeApplication.java
@@ -0,0 +1,15 @@
+package com.mockmock.databridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//https://www.youtube.com/watch?v=AalcVuKwBUM
+@SpringBootApplication
+public class DatabridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabridgeApplication.class, args);
+    }
+
+}
